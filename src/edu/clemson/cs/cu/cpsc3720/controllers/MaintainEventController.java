995100c72afeffde85d53cb7cebc4f66ca0e8592
Jared Klingenberger <klinge2+git@g.clemson.edu>
2014-10-14 20:34:09 +0000

    Modified names to Heats and Events controller

---
+++ src/edu/clemson/cs/cu/cpsc3720/controllers/MaintainEventController.java
@@ -0,0 +1,21 @@
+package edu.clemson.cs.cu.cpsc3720.controllers;
+
+public class MaintainEventController {
+
+	public void createEvent() {
+
+	}
+
+	public void readEvent() {
+
+	}
+
+	public void updateEvent() {
+
+	}
+
+	public void deleteEvent() {
+
+	}
+
+}
