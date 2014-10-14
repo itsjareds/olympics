cb02f2fe0dec0b404b97ce7a42a3a1017083f6af
Jared Klingenberger <klinge2+git@g.clemson.edu>
2014-10-14 20:27:08 +0000

    Split events and heats controllers

---
+++ src/edu/clemson/cs/cu/cpsc3720/controllers/MaintainHeatsController.java
@@ -0,0 +1,21 @@
+package edu.clemson.cs.cu.cpsc3720.controllers;
+
+public class MaintainHeatsController {
+
+	public void createHeat() {
+
+	}
+
+	public void readHeat() {
+
+	}
+
+	public void updateHeat() {
+
+	}
+
+	public void deleteHeat() {
+
+	}
+
+}
